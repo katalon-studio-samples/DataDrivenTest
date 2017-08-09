e2bb06b1bfdc95cf3259742e3d7e07ec68021039
Vinh Nguyen <vinhnguyen@kms-technology.com>
2017-08-09 11:23:16 +0700

    Initialize project

---
+++ Object Repository/Make Appointment Page/btn_Book Appointment.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_Book Appointment</name>
+   <tag></tag>
+   <elementGuidId>9c7481d3-c45e-4a20-b76d-7912a097b673</elementGuidId>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>btn-book-appointment</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Test Suites/Data Driven Test Suite.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Data Driven Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <lastRun>2017-08-09T11:04:39</lastRun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>741b6a0c-fdf2-49e6-936e-e1a021ee9458</testSuiteGuid>
+   <testCaseLink>
+      <guid>736ab5fe-eae9-4b17-8923-0bb6b2353abf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Web Application/Data Driven Test case</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>4b5a4db0-ad48-4601-b1ba-5229dfd5c507</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/Login Test Data/Demo Account</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>4b5a4db0-ad48-4601-b1ba-5229dfd5c507</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>3fce98b0-8ab8-451d-a18d-9f7ae6fd520f</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>4b5a4db0-ad48-4601-b1ba-5229dfd5c507</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>ae1da9f3-dfc2-47bb-8485-2b1a858f44e4</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
